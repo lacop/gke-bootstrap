d3e238f138ae267825ab1c54d4ebb7f28b890737
Laco Papay <lacop@lacop.net>
2024-04-13 19:41:23 +0200

    Add gcloud to dev environment

--- flake.nix
+++ flake.nix
@@ -10,19 +10,24 @@
 
     # 3.14.3
     helm_dep.url = "github:NixOS/nixpkgs/a3ed7406349a9335cb4c2a71369b697cecd9d351";
+
+    # 467.0.0
+    gcloud_dep.url = "github:NixOs/nixpkgs/a3ed7406349a9335cb4c2a71369b697cecd9d351";
   };
 
-  outputs = { self, flake-utils, terraform_dep, helm_dep }@inputs :
+  outputs = { self, flake-utils, terraform_dep, helm_dep, gcloud_dep }@inputs :
     flake-utils.lib.eachDefaultSystem (system:
     let
       terraform_dep = import inputs.terraform_dep { inherit system; config.allowUnfree = true; };
       helm_dep = inputs.helm_dep.legacyPackages.${system};
+      gcloud_dep = inputs.gcloud_dep.legacyPackages.${system};
     in
     {
       devShells.default = helm_dep.mkShell {
         packages = [
           terraform_dep.terraform
           helm_dep.kubernetes-helm
+          gcloud_dep.google-cloud-sdk
         ];
 
         shellHook = ''
@@ -30,6 +35,7 @@
           echo -e "\033[0;31m-------------\033[0m"
           terraform version | head -n 1
           echo "Helm $(helm version --short)"
+          gcloud version | head -n 1
           echo -e "\033[0;31m-------------\033[0m"
           export PS1="nix[cloud]> "
         '';
